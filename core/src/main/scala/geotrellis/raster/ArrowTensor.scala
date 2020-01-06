03c320591182bc54561c0f61392f4a81f298f074
jpolchlo <jpolchlopek@azavea.com>
2020-01-06 09:54:36 -0500

    Add infrastructure for simple local binary operations

--- core/src/main/scala/geotrellis/raster/ArrowTensor.scala
+++ core/src/main/scala/geotrellis/raster/ArrowTensor.scala
@@ -6,12 +6,13 @@ import java.nio.channels.Channels
 
 import com.google.flatbuffers.FlatBufferBuilder
 import org.apache.arrow.flatbuf.{Buffer, Tensor, TensorDim, Type}
-import org.apache.arrow.memory.RootAllocator
+import org.apache.arrow.memory.{BufferAllocator, RootAllocator}
 import org.apache.arrow.vector.ipc.{ReadChannel, WriteChannel}
 import org.apache.arrow.vector.ipc.message.MessageSerializer
 import org.apache.arrow.vector.types.FloatingPointPrecision
 import org.apache.arrow.vector.types.pojo.{ArrowType, Field, FieldType, Schema}
 import org.apache.arrow.vector.{Float8Vector, VectorSchemaRoot}
+import spire.syntax.cfor._
 
 import scala.collection.JavaConverters._
 
@@ -20,6 +21,46 @@ case class ArrowTensor(val vector: Float8Vector, val shape: Seq[Int]) extends Ce
   def rows = shape(1)
   def cols = shape(2)
   val cellType = DoubleCellType
+
+  // TODO: Figure out how to work this crazy thing
+  // def copy(implicit alloc: BufferAllocator) = {
+  //   val n = vector.getValueCount
+  //   val copied = new Float8Vector("array", alloc)
+  //   val tp = vector.makeTransferPair(copied)
+  //   tp.copyValueSafe(0, n)
+  //   copied.copyFromSafe(0, n, vector)
+  //   copied.setValueCount(n)
+  //   ArrowTensor(copied, shape)
+  // }
+
+  def map(fn: Double => Double): ArrowTensor = {
+    val n = vector.getValueCount
+    val result = new Float8Vector("array", ArrowTensor.allocator)
+    result.allocateNew(n)
+    result.setValueCount(n)
+    cfor(0)(_ < n, _ + 1) { i =>
+      if (vector.isSet(i) == 1)
+        result.set(i, fn(vector.get(i)))
+    }
+    ArrowTensor(result, shape)
+  }
+
+  def zipWith(other: ArrowTensor)(fn: (Double, Double) => Double): ArrowTensor = {
+    if (other.shape != shape)
+      throw new IllegalArgumentException(s"Cannot zip tensors of differing sizes.  Got arguments of shape ${shape.mkString("×")} and ${other.shape.mkString("×")}")
+
+    val n = vector.getValueCount
+    val result = new Float8Vector("array", ArrowTensor.allocator)
+    result.allocateNew(n)
+    result.setValueCount(n)
+    cfor(0)(_ < n, _ + 1){ i =>
+      if (vector.isNull(i) || other.vector.isNull(i))
+        result.setNull(i)
+      else
+        result.set(i, fn(vector.get(i), other.vector.get(i)))
+    }
+    ArrowTensor(result, shape)
+  }
 
   /** Write Tensor to buffer, return offset of Tensor object in that buffer */
   def writeTensor(bufferBuilder: FlatBufferBuilder): Int = {
@@ -83,6 +124,8 @@ case class ArrowTensor(val vector: Float8Vector, val shape: Seq[Int]) extends Ce
 }
 
 object ArrowTensor {
+  val allocator = new RootAllocator(Long.MaxValue)
+
   val schema: Schema = {
     val fieldArr = new Field(
       "array",
@@ -93,7 +136,6 @@ object ArrowTensor {
   }
 
   def fromArray(arr: Array[Double], shape: Int*): ArrowTensor = {
-    val allocator = new RootAllocator(Long.MaxValue)
     val root = VectorSchemaRoot.create(schema, allocator)
     val vec = new Float8Vector("array", allocator)
     vec.allocateNew(arr.length)
@@ -103,8 +145,19 @@ object ArrowTensor {
     new ArrowTensor(vec, shape.toArray)
   }
 
+  def fill(v: Double, shape: Int*)(implicit alloc: BufferAllocator): ArrowTensor = {
+    val vec = new Float8Vector("array", alloc)
+    val shp = shape.toSeq
+    val n = shp.product
+    vec.allocateNew(n)
+    vec.setValueCount(n)
+    cfor(0)(_ < n, _ + 1){ i =>
+      vec.set(i, v)
+    }
+    ArrowTensor(vec, shp)
+  }
+
   def fromArrowMessage(bytes: Array[Byte]): ArrowTensor = {
-    val allocator = new RootAllocator(Integer.MAX_VALUE)
     val is = new ByteArrayInputStream(bytes)
     val channel = Channels.newChannel(is)
     val readChannel = new ReadChannel(channel)
